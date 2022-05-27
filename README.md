d82e8b85d13158d7acf8a10c21b89e76e834a6c6
yellowsea <yunhai0644@163.com>
2022-05-27 11:00:08 +0800

    chore: update

--- README.md
+++ README.md
@@ -3,3 +3,39 @@
 
 - 自动执行
 - 使用JS创建Issuse
+
+
+
+### 1. 使用脚本
+
+```js
+// index.js
+
+// 使用了 octokit 第三方库 ： https://www.npmjs.com/package/octokit
+
+const { Octokit } = require("@octokit/rest");
+
+const octokit = new Octokit({ auth: `****` });
+
+octokit.rest.issues.create({
+  owner: "yellowsae",  // 作者
+  repo: "try_github_action",  // 对应的仓库
+  title: "Github Action 2",  // 标题
+  body: "test2"  // 内容
+});
+```
+
+
+
+
+### 2. 创建 Action 指定 index.js
+- 参考： https://docs.github.com/en/actions/creating-actions/creating-a-javascript-action
+1. 创建 `action.yml`
+
+```yml
+
+```
+
+
+
+### 3. 配置 Github 的自动化 Action
--- index.js
+++ index.js
@@ -4,17 +4,38 @@
 
 // 使用了 octokit 第三方库 ： https://www.npmjs.com/package/octokit
 
+const { Octokit } = require("@octokit/rest");
+
+
 // Create a personal access token at https://github.com/settings/tokens/new?scopes=repo
 
 // TODO: auth : 为github的 中创建的 token 
-const octokit = new Octokit({ auth: `personal-access-token123` });
+// const octokit = new Octokit({ auth: `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` });
+
 
 // octokit  api  create issuse
 
 
+// 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
+
+const octokit = new Octokit({ auth: `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` });
+
 octokit.rest.issues.create({
-  owner: "octocat",  // 作者
-  repo: "hello-world",  // 对应的仓库
-  title: "Hello world from " + slug,
+  owner: "yellowsae",  // 作者
+  repo: "try_github_action",  // 对应的仓库
+  title: "Github Action 2",  // 标题
+  body: "test2"  // 内容
 });
 
+
+// const octokit = new Octokit();
+
+// // Compare: https://docs.github.com/en/rest/reference/repos/#list-organization-repositories
+// octokit.rest.repos
+//   .listForOrg({
+//     org: "octokit",
+//     type: "public",
+//   })
+//   .then(({ data }) => {
+//     // handle data
+//   });
